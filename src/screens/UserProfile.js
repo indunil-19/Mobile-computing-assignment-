a724af9e05a081c898daeb567390e958a9892765
Rahal12345 <rahalathukorala@gmail.com>
2023-02-21 12:23:11 +0530

    user profile

--- src/screens/UserProfile.js
+++ src/screens/UserProfile.js
@@ -1,11 +1,42 @@
-import React, { Component } from "react";
-import { Text, View, StyleSheet, Alert } from "react-native";
 import ButtonComponent from "../components/ButtonComponent";
-import { auth } from "../../firebase";
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  Image,
+  StyleSheet,
+  ScrollView,
+  RefreshControl,
+  Alert,
+} from "react-native";
+import { Button } from "react-native-paper";
+import TextField from "../components/TextBox";
+import { database, auth } from "../../firebase";
 export default class UserProfile extends Component {
   constructor(props) {
     super(props);
   }
+  async componentDidMount() {
+    this.loadData();
+  }
+
+  async loadData() {
+    await database
+      .ref(`/users/${auth.currentUser.uid}`)
+      .once("value")
+      .then((snapshot) => {
+        console.log(snapshot.val());
+        this.setState({
+          email: snapshot.val().email,
+          firstname: snapshot.val().firstname,
+          lastname: snapshot.val().lastname,
+          type: snapshot.val().type,
+          licence: snapshot.val().licence,
+        });
+      })
+      .catch((error) => console.log(error));
+  }
+
   //on log out request
   logout() {
     //prompt confirmation from user
@@ -28,6 +59,50 @@ export default class UserProfile extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <View style={styles.content}>
+          <Image
+            source={require("../../assets/driver.png")}
+            alignSelf="center"
+            style={styles.image}
+          />
+          <Text style={styles.logo}>User Details</Text>
+          <TextField
+            icon="user"
+            type="antdesign"
+            text={"First name"}
+            value={this.state?.firstname}
+            editable={false}
+          />
+          <TextField
+            icon="user"
+            type="antdesign"
+            text={"Last name"}
+            value={this.state?.lastname}
+            editable={false}
+          />
+          <TextField
+            icon="calendar"
+            type="antdesign"
+            text={"Email"}
+            value={this.state?.email}
+            editable={false}
+          />
+          <TextField
+            icon="car"
+            type="antdesign"
+            text={"Licence"}
+            value={this.state?.licence}
+            editable={false}
+          />
+          <TextField
+            icon="calendar"
+            type="antdesign"
+            text={"Type"}
+            value={this.state?.type}
+            editable={false}
+          />
+        </View>
+
         <ButtonComponent
           icon="logout"
           type="antdesign"
@@ -42,12 +117,25 @@ export default class UserProfile extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    backgroundColor: "#EFF1F3",
+  },
+  content: {
+    flex: 1,
+    alignSelf: "center",
+    width: "90%",
+    paddingTop: "10%",
+  },
+  logo: {
+    fontWeight: "bold",
+    fontSize: 35,
+    color: "#2E6CB5",
+    marginBottom: 5,
+    textAlign: "center",
   },
   image: {
-    flex: 1,
-    height: 150,
+    // flex: 2,
     width: 150,
+    height: 150,
     resizeMode: "contain",
   },
 });
--- src/screens/agent/AgentUserProfile.js
+++ src/screens/agent/AgentUserProfile.js
@@ -1,11 +1,43 @@
-import React, { Component } from "react";
-import { Text, View, StyleSheet, Alert } from "react-native";
 import ButtonComponent from "../../components/ButtonComponent";
-import { auth } from "../../../firebase";
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  Image,
+  StyleSheet,
+  ScrollView,
+  RefreshControl,
+  Alert,
+} from "react-native";
+import { Button } from "react-native-paper";
+import TextField from "../../components/TextBox";
+import { database, auth } from "../../../firebase";
+import AsyncImage from "../../components/AsyncImage";
 export default class AgentUserProfile extends Component {
   constructor(props) {
     super(props);
   }
+  async componentDidMount() {
+    this.loadData();
+  }
+
+  async loadData() {
+    await database
+      .ref(`/users/${auth.currentUser.uid}`)
+      .once("value")
+      .then((snapshot) => {
+        console.log(snapshot.val());
+        this.setState({
+          ID: snapshot.val().ID,
+          email: snapshot.val().email,
+          firstname: snapshot.val().firstname,
+          lastname: snapshot.val().lastname,
+          type: snapshot.val().type,
+        });
+      })
+      .catch((error) => console.log(error));
+  }
+
   //on log out request
   logout() {
     //prompt confirmation from user
@@ -28,6 +60,51 @@ export default class AgentUserProfile extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <View style={styles.content}>
+          <Image
+            source={require("../../../assets/agent.png")}
+            alignSelf="center"
+            style={styles.image}
+          />
+          <Text style={styles.logo}>User Details</Text>
+
+          <TextField
+            icon="user"
+            type="antdesign"
+            text={"ID"}
+            value={this.state?.ID}
+            editable={false}
+          />
+          <TextField
+            icon="user"
+            type="antdesign"
+            text={"First name"}
+            value={this.state?.firstname}
+            editable={false}
+          />
+          <TextField
+            icon="user"
+            type="antdesign"
+            text={"Last name"}
+            value={this.state?.lastname}
+            editable={false}
+          />
+          <TextField
+            icon="calendar"
+            type="antdesign"
+            text={"Email"}
+            value={this.state?.email}
+            editable={false}
+          />
+          <TextField
+            icon="calendar"
+            type="antdesign"
+            text={"Type"}
+            value={this.state?.type}
+            editable={false}
+          />
+        </View>
+
         <ButtonComponent
           icon="logout"
           type="antdesign"
@@ -42,12 +119,25 @@ export default class AgentUserProfile extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    backgroundColor: "#EFF1F3",
+  },
+  content: {
+    flex: 1,
+    alignSelf: "center",
+    width: "90%",
+    paddingTop: "10%",
+  },
+  logo: {
+    fontWeight: "bold",
+    fontSize: 35,
+    color: "#2E6CB5",
+    marginBottom: 5,
+    textAlign: "center",
   },
   image: {
-    flex: 1,
-    height: 150,
+    // flex: 2,
     width: 150,
+    height: 150,
     resizeMode: "contain",
   },
 });
